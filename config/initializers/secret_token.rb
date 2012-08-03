cc003964bf55587cccdf018a0a9d2f3148e1dbaa
Jenner LaFave <jenner@jfave.com>
2012-08-02 17:00:04 -0700

    New rails; added gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nifmon::Application.config.secret_token = 'df3ab3c133e9c3af431fb829a63985ec911addf3595e2633c5908f9a73fa1dc1ab161e60d04c2a091d50891a07162a0d513a5cdfa3a1b21549c4cb615b55ea77'
