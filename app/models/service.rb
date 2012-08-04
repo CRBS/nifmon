c7294bc0dfc533fcdd579cdd9e9f40dd6700856e
Jenner LaFave <jenner@jfave.com>
2012-08-03 19:18:00 -0700

    Wrote unit tests for Service and fixed some bugs

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ActiveRecord::Base
   include Net
-  attr_accessible :description, :expectation, :last_poll, :name, :poll_interval, :uri, :valid, :api_format, :user_agent, :cookies
+  attr_accessible :description, :expectation, :name, :poll_interval, :uri, :api_format, :user_agent, :cookies
   has_many :polls, :dependent => :destroy
 
   def poll_webservice
@@ -10,40 +10,44 @@ class Service < ActiveRecord::Base
     poll.response_text = poll.response[:body]
     poll.healthy = (poll.response_time > 0 && poll.response[:valid])
     poll.save
-    @polls << poll
+    self.polls << poll
   end
 
   def get_response
     response = {}
     response[:avg_time] = get_avg_response_time
-    response[:body] = HTTParty.get(@uri).body
-    response[:valid] = validate_response(response[:body], @expectation)
+    response[:body] = HTTParty.get(self.uri).body
+    response[:valid] = validate_response(response[:body], self.expectation)
     return response
   end
 
   def get_avg_response_time
-    service_uri = URI.parse(@uri)
+    service_uri = URI.parse(self.uri)
     pings = []
     5.times do |i|
       before_time = Time.now
       begin
         http = Net::HTTP.new(service_uri.host, service_uri.port)
+        http.open_timeout = 1 
         http.read_timeout = 2
-        if @uri.scheme == 'https'
+        if service_uri.scheme == 'https'
           http.use_ssl = true
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         end
-        response, data = http.request(Net::HTTP::Get.new(uri.request_uri, request_headers))
+        response, data = http.request(Net::HTTP::Get.new(service_uri.request_uri, request_headers))
+        pings[i] = (Time.now - before_time) * 1000
       rescue Timeout::Error => e
         pings[i] = -1
+      rescue SocketError => e
+        return 0
       end
-      pings[i] = (Time.now - before_time) * 1000
     end
-    pings.delete_if { |p| p < 1 }
+    pings.delete_if { |p| p < 1 }    
     if pings.length > 0
       return (pings.reduce(:+) / pings.length)
     else
       return 0
+    end
   end
 
   # TODO: Make this method smarter
@@ -53,8 +57,8 @@ class Service < ActiveRecord::Base
 
   def request_headers
     header = {}
-    header['User-Agent'] = @user_agent or 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3'
-    header['Cookies'] = @cookies if @cookies
+    header['User-Agent'] = self.user_agent || 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3'
+    header['Cookies'] = self.cookies if self.cookies
     return header
   end
 end
--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should report average response time" do
+    service = services(:federation_query)
+    time = service.get_avg_response_time
+    assert (time > 0), "Did not return a valid response time for federation_query. Output: #{time}"
+  end
+
+  test "avg_response_time should return 0 if unable to open connection to host" do
+    service = services(:nxdomain)
+    time = service.get_avg_response_time
+    assert (time == 0), "Returned #{time} instead of 0 for a non-existent URI."
+  end
+
+  test "avg_response_time should return 0 if connection times out" do
+    service = services(:noweb)
+    time = service.get_avg_response_time
+    assert (time == 0), "Return #{time} instead of 0 for a downed web service."
+  end
+
+  test "poll_webservice should create a new Poll if successful" do
+    service = services(:federation_query)
+    before_polls = service.polls.size
+    service.poll_webservice
+    assert (service.polls.size == before_polls + 1), "poll_webservice did not create a new Poll"
+  end
+
+  test "response should not be valid if expectation fails" do
+    service = services(:bad_expectation)
+    r = service.get_response
+    assert !r[:valid], "Response was valid even with a failed expectation. Response: #{r[:body]}"
+  end
 end
