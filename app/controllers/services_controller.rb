023d0e6e140054680f45a5294a501707d4405064
Jenner LaFave <jenner@jfave.com>
2012-08-29 19:48:41 -0700

    Wrote Services controller and some basic views for it

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,53 @@
+class ServicesController < ApplicationController
+  def index
+    @title = 'All services'
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find params[:id]
+    @service.poll_webservice
+  end
+
+  def new
+    @title = 'Add a new service'
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new params[:service]
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      flash[:error] = 'There was a problem creating a new service.'
+      flash[:errors] = @service.errors
+      redirect_to :action => 'new'
+    end
+  end
+
+  def edit
+    @service = Service.find params[:id]
+  end
+
+  def update
+    @service = Service.find params[:id]
+    if @service.update_attributes(params[:service])
+      redirect_to service_path(@service)
+    else
+      flash[:error] = "There was a problem saving your changes to #{@service.name}."
+      flash[:errors] = @service.errors
+      redirect_to :action => 'edit'
+    end
+  end
+
+  def destroy
+    @service = Service.find params[:id]
+    if @service.destroy
+      redirect_to :action => 'index'
+    else
+      flash[:error] = "There was a problem deleting the service #{@service.name}."
+      flash[:errors] = @service.errors
+      redirect_to :action => 'show'
+    end
+  end
+end
