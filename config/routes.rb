4dae37125172a2fb8882b5e7e2625e238fb3e9d2
Jenner LaFave <jenner@jfave.com>
2012-08-29 18:48:43 -0700

    Added Dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Nifmon::Application.routes.draw do
   resources :polls, :services
-  resource :dashboard
+
+  match 'dashboard' => 'dashboard#show', :as => :dashboard, :via => :get
+  match 'dashboard' => 'dashboard#update', :via => :put
+  match 'dashboard/edit' => 'dashboard#edit', :as => :edit_dashboard, :via => :get
+
 end
